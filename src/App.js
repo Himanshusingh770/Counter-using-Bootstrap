7262cc94b0b10e4cb33af0bebba32d1d45ae754d
Himanshu <himanshu.singh@innotechteam.in>
2024-09-27 19:10:53 +0530

    Change the name of state

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import './App.css';
 
 const App = () => {
   const [timer, setTimer] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
+  const [isTimerRunning, setisTimerRunning] = useState(false);
 
   useEffect(() => {
     let interval;
-    if (isRunning) {
+    if (isTimerRunning) {
       interval = setInterval(() => {
         setTimer((prev) => prev + 1);
       }, 1000);
-    } else if (!isRunning && timer !== 0) {
+    } else if (!isTimerRunning && timer !== 0) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isRunning]);
+  }, [isTimerRunning]);
 
   const counterTimer = () => {
-    setIsRunning(!isRunning);
+    setisTimerRunning(!isTimerRunning);
   };
 
   const resetTimer = () => {
-    setIsRunning(false);
+    setisTimerRunning(false);
     setTimer(0);
   };
 
@@ -59,7 +59,7 @@ const App = () => {
             <div className="modal-body">
               <p>Timer: {timer} seconds</p>
               <button className="btn btn-success" onClick={counterTimer}>
-                {isRunning ? 'Pause' : 'Start'}
+                {isTimerRunning ? 'Pause' : 'Start'}
               </button>
               <button className="btn btn-danger m-2" onClick={resetTimer}>
                 Reset
